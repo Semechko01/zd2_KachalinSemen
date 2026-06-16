ab5a3e967658bc246e3834fc77b90616915e53bf
Semechko01 <fasta.goli@gmail.com>
2026-06-16 11:11:30 +0500

    Add files via upload

---
+++ zd2_KachalinSemen — копия/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+namespace zd2_KachalinSemen
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PhoneBook Book = new PhoneBook();
+        public void UpdDataGrid()
+        {
+            datagrid1.ItemsSource = null;
+            datagrid1.Items.Clear();
+            datagrid1.ItemsSource = Book.GetListContact(); 
+
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            PhoneBookLoader.Load(Book, $"contacts.csv");
+            UpdDataGrid();
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            PhoneBookLoader.Save(Book, $"contacts.csv");
+            UpdDataGrid();
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            datagrid1.ItemsSource = null;
+            datagrid1.ItemsSource = Book.SearchContactName(SearchName.Text);
+            
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            if(NameAdded.Text.Length == 0 || PhoneAdded.Text.Length == 0)
+            {
+                MessageBox.Show("Строка с именем или телефоном пуста");
+                return;
+            }
+            Book.DeleteContact(NameAdded.Text, PhoneAdded.Text);
+            UpdDataGrid();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            if (NameAdded.Text.Length == 0 || PhoneAdded.Text.Length == 0)
+            {
+                MessageBox.Show("Строка с именем или телефоном пуста");
+                return;
+            }
+            var FalseChaars = PhoneAdded.Text.ToCharArray().ToList().Where(T => !Char.IsDigit(T)).ToList();
+            if(FalseChaars.Count > 0)
+            {
+                MessageBox.Show("Номер телефона может иметь только цифры");
+                return;
+            }
+            Book.AddContact(NameAdded.Text, PhoneAdded.Text);
+            UpdDataGrid();
+        }
+    }
+}
---
+++ zd2_KachalinSemen — копия/PhoneBook.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zd2_KachalinSemen
+{
+    class PhoneBook
+    {
+        List<Contact> ListContact = new List<Contact>();
+        public List<Contact> SearchContactName(string Name)
+        {
+            var contanct = ListContact.Where(t => t.Name.Contains(Name)).ToList();
+            return contanct;
+        }
+        public List<Contact> SearchContactPhone(string Phone)
+        {
+            var contanct = ListContact.Where(t => t.Phone == Phone).ToList();
+            return contanct;
+        }
+        public void AddContact(Contact contact)
+        {
+            ListContact.Add(contact);
+        }
+        public void AddContact(string name, string phone)
+        {
+            Contact num1 = new Contact();
+            num1.Name = name;
+            num1.Phone = phone;
+            ListContact.Add(num1);
+        }
+        public void DeleteContact(string name)
+        {
+            var contanct = ListContact.Where(t => t.Name == name).ToList();
+            if (contanct.Count > 0)
+                ListContact.Remove(contanct[0]);
+        }
+        public void DeleteContact(string name,string phone)
+        {
+            var contanct = ListContact.Where(t => t.Name == name && t.Phone == phone).ToList();
+            if(contanct.Count > 0)
+                ListContact.Remove(contanct[0]);
+        }
+        public void DeleteContact(Contact num)
+        {
+            ListContact.Remove(num);
+        }
+        public List<Contact> GetListContact()
+        {
+            return ListContact;
+        }
+        public void ListBookClear()
+        {
+            ListContact.Clear();
+        }
+    }
+}
